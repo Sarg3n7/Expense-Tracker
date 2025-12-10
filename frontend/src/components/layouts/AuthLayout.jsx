dc8cb2436a6ce9a86d153e7450e3465f8a2592ed
Sarg3n7 <shashwatshivam04@gmail.com>
2025-12-10 21:13:43 +0530

    Defined app routes

---
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AuthLayout = ({ children }) => {
+  return <div>
+    <div className='w-screen h-screen'>
+        <h2 className='text-lg font-medium'></h2>
+    </div>
+  </div>
+  
+}
+
+export default AuthLayout
