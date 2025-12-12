15603f3b78b683c5e73e39c2f3e6db35b51ed3a6
Sarg3n7 <shashwatshivam04@gmail.com>
2025-12-12 16:08:08 +0530

    Established successfull connection with mongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import connectDB from './config/db.js';
 
 dotenv.config({
     path: './.env'
 })
+
+const app = express();
 
 
 app.use(
@@ -17,6 +20,8 @@ app.use(
 )
 
 app.use(express.json())
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
