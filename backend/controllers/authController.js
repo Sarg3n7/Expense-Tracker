4fce5759c032bfcabdb5d3e7b544f2dabfa7b30a
Sarg3n7 <shashwatshivam04@gmail.com>
2025-12-17 01:26:23 +0530

    implemented logic to get user information

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -68,7 +68,18 @@ const loginUser = async(req, res) => {
 
 //Get user information
 const getUserInfo = async(req, res) => {
+    try {
+        const user = await User.findOne(req.user.id).select("-password")
 
+        if(!user){
+            return res.status(404).json({message: "User not found"})
+        }
+        res.status(200).json(user)
+    } catch (err) {
+        res
+        .status(500)
+        .json({message: "Error registering user", error: err.message})
+    }
 }
 
 
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,13 @@
 import { registerUser, loginUser, getUserInfo } from '../controllers/authController.js'
 import express from 'express';
+import {protect} from '../middleware/authMiddleware.js'
 
 
 const router = express.Router()
 
 router.post("/register", registerUser)
 router.post("/login", loginUser)
-// router.post("/getUser", protect, getUserInfo)
+router.post("/getUser", protect, getUserInfo)
 
 
 
