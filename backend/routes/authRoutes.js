57d7e2dc96f0b77a2eb0ff3a8b23f377712542e3
Sarg3n7 <shashwatshivam04@gmail.com>
2025-12-17 01:37:58 +0530

    fixed getUserInfo logic

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router()
 
 router.post("/register", registerUser)
 router.post("/login", loginUser)
-router.post("/getUser", protect, getUserInfo)
+router.get("/getUser", protect, getUserInfo)
 
 
 
