a1873faedc0c4c6961cf6ebd4abf6a646a03e0e3
Sarg3n7 <shashwatshivam04@gmail.com>
2025-12-18 03:03:03 +0530

    implement image upload functionality with multer

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,16 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
+import path from 'path'
+import { fileURLToPath } from 'url';
+
 
 dotenv.config({
     path: './.env'
 })
 
 const app = express();
+
 
 
 app.use(
@@ -25,6 +29,14 @@ app.use(express.json())
 connectDB();
 
 app.use("/api/v1/auth", authRoutes)
+
+//Serve uploads folder
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// app.use('/uploads',express.static(path.join(__dirname, 'uploads')))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
