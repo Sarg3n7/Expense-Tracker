e0281bb5cfcf926edb2201e633a428f5462aa914
Sarg3n7 <shashwatshivam04@gmail.com>
2025-12-26 02:40:42 +0530

    refactor CustomBarChart to accept dynamic xKey and labelKey props

--- frontend/src/components/Charts/CustomBarChart.jsx
+++ frontend/src/components/Charts/CustomBarChart.jsx
@@ -11,7 +11,7 @@ import {
     Cell,
 } from 'recharts';
 
-const CustomBarChart = ({data}) => {
+const CustomBarChart = ({data =[] , xKey, labelKey}) => {
 
     //Function to alternate colors
     const getBarColor = (index) => {
@@ -22,9 +22,9 @@ const CustomBarChart = ({data}) => {
         if (active && payload && payload.length){
             return(
                 <div className='bg-white shadow-md rounded-lg p-2 border border-gray-300'>
-                    <p className='text-xs font-semibold text-purple-800 mb-1'>{payload[0].payload.category}</p>
+                    <p className='text-xs font-semibold text-purple-800 mb-1'>{payload[0].payload[labelKey]}</p>
                     <p className='text-sm text-gray-600'>
-                        Amount: <span className='text-sm font-medium text-gray-900'>₹{payload[0].payload.amount}</span>
+                        Amount:{" "} <span className='text-sm font-medium text-gray-900'>₹{payload[0].payload.amount}</span>
                     </p>
                 </div>
             )
@@ -37,7 +37,7 @@ const CustomBarChart = ({data}) => {
         <ResponsiveContainer width="100%" height={300}>
             <BarChart data={data}>
                 <CartesianGrid stroke='none'/>
-                <XAxis dataKey='category' tick={{ fontSize: 12, fill: "#555"}} stroke='none'/>
+                <XAxis dataKey={xKey} tick={{ fontSize: 12, fill: "#555"}} stroke='none'/>
                 <YAxis tick={{fontSize:12, fill: '#555'}} stroke='none'/>
                 <Tooltip content={CustomToolTip}/>
                 <Bar
