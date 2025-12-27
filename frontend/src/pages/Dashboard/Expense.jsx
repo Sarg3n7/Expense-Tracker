933bf54d25c2fa859e76c11f74e7a373655c848f
Sarg3n7 <shashwatshivam04@gmail.com>
2025-12-28 00:46:55 +0530

    Implemented form UI and integrated the API to add expenses

--- frontend/src/pages/Dashboard/Expense.jsx
+++ frontend/src/pages/Dashboard/Expense.jsx
@@ -1,9 +1,119 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { API_PATHS } from "../../utils/apiPaths";
+import toast from "react-hot-toast";
+import axiosInstance from "../../utils/axiosInstance";
+import ExpenseOverview from "../../components/Expense/ExpenseOverview";
+import Modal from "../../components/Modal";
+import AddExpenseForm from "../../components/Expense/AddExpenseForm";
 
 const Expense = () => {
-  return (
-    <div>Expense</div>
-  )
-}
 
-export default Expense
+  const [expenseData, setExpenseData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState({
+    show: false,
+    data: null,
+  });
+
+  const [openAddExpenseModal, setOpenAddExpenseModal] = useState(false);
+
+
+  //Get all expense details
+  const fetchExpenseDetails = async()=>{
+    if(loading) return
+    setLoading(true)
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.EXPENSE.GET_ALL_EXPENSE}`
+      )
+
+      if(response.data){
+        setExpenseData(response.data)
+      }
+    } catch (error) {
+      console.error("Something went wrong. Please try again.", error)
+    } finally{
+      setLoading(false)
+    }
+  }
+
+
+
+  //Handle Add Expense
+  const handleAddExpense = async(expense)=>{
+    const {category, amount, date, icon} = expense
+
+    //validation checks
+    if(!category.trim()){
+      toast.error("Category is required")
+      return
+    }
+
+    if(!amount || isNaN(amount) || Number(amount) <= 0){
+      toast.error("Amount should be valid number greater than 0.")
+      return
+    }
+
+    if(!date){
+      toast.error("Date is required")
+    }
+
+    try {
+      await axiosInstance.post(API_PATHS.EXPENSE.ADD_EXPENSE, {
+        category,
+        amount,
+        date,
+        icon,
+      })
+
+      setOpenAddExpenseModal(false)
+      toast.success("Expense added successfully")
+      fetchExpenseDetails()
+    } catch (error) {
+      console.error("Error adding expense:", error.response?.data?.message || error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchExpenseDetails()
+
+    return () => {}
+  }, [])
+
+  return (
+    <DashboardLayout activeMenu="Expense">
+      <div className="my-5 mx-auto">
+        <div className="">
+          <div className="">
+            <ExpenseOverview
+              transactions={expenseData}
+              onExpenseIncome = {() => setOpenAddExpenseModal(true)} 
+            />
+          </div>
+
+          <ExpenseList
+            transactions={expenseData}
+            onDelete={(id) => {
+              setOpenDeleteAlert({show: true, data: id})
+            }}
+
+            onDownload = {handleDownloadExpenseDetails}
+          />
+        </div>
+
+
+        <Modal
+          isOpen={openAddExpenseModal}
+          onClose={() => setOpenAddExpenseModal(false)}
+          title='Add Expense'
+        >
+          <AddExpenseForm onAddExpense ={handleAddExpense}/>
+        </Modal>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default Expense;
